fa7803e2dc4014daf8b5c115492835e56f95d0de
DT7Solutions <developer@dt7solutions.com>
2023-02-27 15:09:30 +0530

    adding blogs

---
+++ js/blogs.js
@@ -0,0 +1,17 @@
+// let items = $('.blog-grid .item')
+// let numItems = items.length;
+// let perpage = 6;
+
+// items.slice(perpage).hide();
+// $('.pagination-ourter').pagination({
+//     items:numItems,
+//     itemsOnPage : perpage,
+//     prevText :'Pre',
+//     nextText:'Next',
+//     onPageClick :function(pageNumber){
+//         let showForm = perpage * (pageNumber - 1);
+//         let showTo = showForm + perpage;
+//         items.hide().slice(showForm, showTo).show();
+
+//     }
+// })
