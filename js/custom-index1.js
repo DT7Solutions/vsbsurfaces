ab50f598503197a6a2b48e5d47e68e540d0154c9
DT7Solutions <developer@dt7solutions.com>
2023-04-21 10:09:38 +0530

    first commit

---
+++ js/custom-index1.js
@@ -0,0 +1,130 @@
+(function($) { "use strict";
+	
+	// --------------------------------------------------
+    // owlCarousel
+    // --------------------------------------------------
+
+    /* natural stones-items */
+    $("#latest-projects-items").owlCarousel({
+        items: 3,
+        itemsCustom : false,
+        loop: true,
+        autoPlay : true,
+        itemsDesktop : [1199, 3],
+        itemsDesktopSmall : [979, 2],
+        itemsTablet : [768, 2],
+        itemsTabletSmall : false,
+        itemsMobile : [479, 1],
+        navigation: true,
+        pagination: false,
+        navigationText: [
+          "<i class='fa fa-angle-left'></i>",
+          "<i class='fa fa-angle-right'></i>"
+        ],
+        slideSpeed : 200,
+    });
+
+      /* engineered stones-items2 */
+      $("#latest-projects-items2").owlCarousel({
+        rtl:false,
+        items: 3,
+        itemsCustom : false,
+        loop: true,
+        autoPlay : true,
+        itemsDesktop : [1199, 3],
+        itemsDesktopSmall : [979, 2],
+        itemsTablet : [768, 2],
+        itemsTabletSmall : false,
+        itemsMobile : [479, 1],
+        navigation: true,
+        pagination: false,
+        navigationText: [
+          "<i class='fa fa-angle-left'></i>",
+          "<i class='fa fa-angle-right'></i>"
+        ],
+        slideSpeed : 400,
+        
+        
+    });
+
+
+    
+
+
+
+    /* latest-news-items */
+    $(".latest-news-items").owlCarousel({
+        items : 1,
+        singleItem:true,    
+        navigation : false,
+        pagination : false,
+        autoPlay : true,
+        navigationText: [
+          "<i class='fa fa-angle-left'></i>",
+          "<i class='fa fa-angle-right'></i>"
+        ],
+        slideSpeed : 400,
+    });    
+
+    // Custom Navigation owlCarousel - ".latest-news-items"
+    $(".latest-next").on("click", function() {
+        $(this).parent().parent().find('.latest-news-items').trigger('owl.next');
+    });
+    $(".latest-prev").on("click", function() {
+        $(this).parent().parent().find('.latest-news-items').trigger('owl.prev');
+    });  
+
+    /*testimonials-slider */
+    $(".testimonials-slider").owlCarousel({
+        items : 1,
+        singleItem:true,    
+        navigation : false,
+        pagination : false,
+        autoPlay : false,
+        slideSpeed : 400,
+    }); 
+
+    // Custom Navigation owlCarousel - ".testimonials-slider"
+    $(".testi-next").on("click", function() {
+        $(this).parent().parent().find('.testimonials-slider').trigger('owl.next');
+    });
+    $(".testi-prev").on("click", function() {
+        $(this).parent().parent().find('.testimonials-slider').trigger('owl.prev');
+    });
+
+    
+})(jQuery); 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+
+
+
+
+
+	
