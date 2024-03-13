d53ebfca10283266ae62c0b4b6d22daf3057a34b
suresh522 <kajasuresh522@gmail.com>
2024-03-13 11:44:24 +0530

    all fixed

--- js/custom-product-single.js
+++ js/custom-product-single.js
@@ -20,6 +20,40 @@
           "<i class='fa fa-angle-right'></i>"
         ],
     });
+
+    $(".our-client-works-slider").owlCarousel({
+      items: 3,
+      itemsCustom : false,
+      itemsDesktop : [1199, 3],
+      itemsDesktopSmall : [979, 2],
+      itemsTablet : [768, 2],
+      itemsTabletSmall : false,
+      itemsMobile : [479, 1],
+      navigation: true,
+      pagination: true,
+      navigationText: [
+        "<i class='fa fa-angle-left'</i>",
+        "<i class='fa fa-angle-right'</i>",
+        
+      ],
+});
+
+
+$(".our-natural-stone-slider").owlCarousel({
+  items:3,
+  itemsCustom:false,
+  itemsDesktop:[1199,3],
+  itemsDesktopSmall:[979, 2],
+  itemsTablet:[768, 2],
+  itemsTabletSmall:false,
+  itemsMobile:[479,1],
+  navigation:true,
+  pagination:true,
+  navigationText:[
+    "<i class='fa fa-angle-left'</i>",
+    "<i class='fa fa-angle-right'</i>",
+  ],
+});
     
 })(jQuery); 
  
